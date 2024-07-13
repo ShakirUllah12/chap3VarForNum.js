632b329a59647caf192e5d6f2824443762d1146f
Shakir Ullah <shakirullahaup@gmail.com>
2024-07-13 16:54:07 +0500

    chap3 done

---
+++ script.js
@@ -0,0 +1,27 @@
+const name="shakir"
+console.log(name);
+
+const myAge="24"
+console.log(myAge);
+
+let mySallary="3000"
+console.log(mySallary);
+
+const myHeight="6";
+console.log(myHeight);
+
+let marks=70;
+console.log(marks + 20);
+
+let chocklateBox=15;
+let newBox=chocklateBox + 5;
+console.log(newBox)
+
+let firstNumber= 1;
+let secondNumber=2;
+console.log(firstNumber+secondNumber);
+
+let firstName="shakir";
+let lastName="khan";
+let fullName=( firstName + " " +lastName);
+console.log(fullName);
